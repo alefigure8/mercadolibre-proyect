d26883d90582c0e1c19308452634b8d1b7e0798f
Alejandro Gomez Nieto <36733780+alefigure8@users.noreply.github.com>
2021-11-07 11:32:19 -0300

    1.1.0

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const app = express();
 
 //API RESPONSE
 const mercadolibre = [{
-        name: 'page',
-        address: 'url',
-        base: ''
-    },
+    name: "Silla Gamer",
+    address: "https://listado.mercadolibre.com.ar/silla-gamer",
+    base: "",
+}, ];
 
-];
+//Return
 const post = [];
 
 //ROUTE
 app.get("/", (req, res) => {
-    res.json("Welcome to my *** Change News API");
+    res.json("API Mercado Libre");
 });
 
 mercadolibre.forEach((ml) => {
@@ -27,25 +27,51 @@ mercadolibre.forEach((ml) => {
         .then((response) => {
             const html = response.data;
             const $ = cheerio.load(html);
-            $('a:contains("***")', html).each((i, el) => {
-                const title = $(el).text();
-                const url = $(el).attr("href");
+            $("div .ui-search-result__content-wrapper", html).each((i, el) => {
+                const title = $(el).find("h2").text();
+                let price = $(el).find(".price-tag-fraction").html();
+                const url = $(el).find(".ui-search-link").attr("href");
+                let id = url.split('#')[0].split('/').slice(-1).join();
+                const opinions = $(el).find(".ui-search-reviews__amount").text();
+                let cuotas = $(el).find(".ui-search-color--LIGHT_GREEN").text();
+                if (cuotas === '') {
+                    cuotas = 'Sin cuotas'
+                }
                 post.push({
+                    id,
                     title,
+                    price,
+                    rating: '',
+                    opinions,
+                    cuotas,
                     url: ml.base + url,
                     source: ml.name,
                 });
             });
         })
+        .then(() => {
+            for (let i in post) {
+                const { url } = post[i]
+                axios
+                    .get(url)
+                    .then(response => {
+                        const html = response.data;
+                        const $ = cheerio.load(html)
+                        post[i].rating = $('.ui-pdp-reviews__rating__summary__average').text()
+                    })
+            }
+        })
         .catch((err) => console.log(err));
 });
 
-app.get("/post", (req, res) => {
-    res.json(post);
+
+app.get("/post", async(req, res) => {
+    //Compare and sort
+    res.json(post.sort((a, b) => b.rating - a.rating));
 });
 
-app.get("/news/:newspaperID", (req, res) => {
-    const id = req.params.newspaperID;
+app.get("/post/:postId", (req, res) => {
+    const id = req.params.postId;
     const ml = mercadolibre.filter((ml) => ml.name === id)[0];
     axios
         .get(ml.address)
@@ -53,11 +79,15 @@ app.get("/news/:newspaperID", (req, res) => {
             const html = response.data;
             const $ = cheerio.load(html);
             const specificpost = [];
-            $('a:contains("***")', html).each((i, el) => {
+            $("div .ui-search-result__content-wrapper", html).each((i, el) => {
                 const title = $(el).text();
+                const price = Number($(el).find(".ui-search-item__group--price"));
                 const url = $(el).attr("href");
                 specificpost.push({
                     title,
+                    price,
+                    rating,
+                    img,
                     url: ml.base + url,
                     source: ml.name,
                 });
